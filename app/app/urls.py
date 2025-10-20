17f105f1fa8da05b8b579ce685671edaf53d08d6
serhii-sudo <sergeyrudenko1992@gmail.com>
2025-10-21 00:15:00 +0300

    chore: added new urlpatterns for favicon.ico

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.templatetags.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url=static('img/favicon.ico'), permanent=True)),
     path('', include('users.urls'))
 ]
